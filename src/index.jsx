6794e3868930fb9282c29584af968e25d6901594
erenkulaksiz <eren@kulaksiz.work>
2021-01-15 05:35:26 +0300

    setup folder structure

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styles from './styles/test.module.scss'
+import './styles/globals.scss'
+//
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+import TodoApp from './modules/todoapp/todoapp.jsx'
 
 function App() {
   return (
     <div className="App">
-        <div className={styles.test}>
-          woah.
+        <div>
+          woah. <FontAwesomeIcon icon={faCoffee} />
+          <TodoApp />
         </div>
     </div>
   );
---
+++ src/modules/todoapp/todoapp.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const todoapp = () => (
+    <div>todoapp module</div>
+);
+
+export default todoapp
