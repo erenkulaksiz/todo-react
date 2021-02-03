3b60e04c46084c2a2cebd9743b0b90ce153517d9
erenkulaksiz <eren@kulaksiz.work>
2021-02-04 01:48:09 +0300

    API

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './styles/globals.scss';
+//API
+import axios from 'axios'
 
 import { createStore } from 'redux';
 import reducer from './reducers/index.js';
@@ -14,46 +16,155 @@ import Card from './modules/card/card.jsx';
 import AddTaskBtn from './modules/addtask/addtask.jsx';
 import AddBanner from './modules/addbanner/addbanner.jsx';
 
-const store = createStore(reducer)
+const store = createStore(reducer);
+
+const apiRoute = "https://5fca12143c1c220016441a5f.mockapi.io/app/api/tasks/";
 
 class TodoApp extends Component {
 
+    refresh = async () => {
+
+        console.log("@refresh");
+        try {
+            await axios.get(apiRoute)
+                .then(res => {
+                    const data = res.data;
+                    //console.log(data);
+                    store.dispatch({ type: 'SET_TASKS', payload: data });
+                })
+                .catch(err => {
+                    console.log(err.response.status);
+                    if (err.response.status === 429) {
+                        throw new Error('429');
+                    }
+                    throw err;
+                })
+        } catch (err) {
+            console.log("%cCant refresh tasks:" + err, "color:red");
+        }
+
+    }
+
+    componentDidMount() {
+        this.refresh();
+    }
+
     render() {
+
+        const storeTasks = store.getState().tasks;
+        const storeEdit = store.getState().edit;
+
+        // Return a task's id depending on index input.
+        /*
+        const findID = (tasks, taskIndex) => {
+            let foundID = -1;
+            tasks.map(function (key, index) {
+                if (index == taskIndex) {
+                    foundID = parseInt(key.id)
+                }
+            })
+            return foundID;
+        }*/
+
+        const findIndex = (tasks, taskId) => {
+            let foundIndex = -1;
+            tasks.map(function (key, index) {
+                if (parseInt(key.id) == taskId) {
+                    foundIndex = index;
+                }
+            })
+            return foundIndex;
+        }
 
         const editMode = (taskId) => {
             console.log("edit mode: " + taskId);
             store.dispatch({ type: 'EDIT_MODE', payload: taskId });
         }
 
-        const addTask = (taskTarget) => {
+        const addTask = async (taskTarget) => {
             console.log("add task click target: " + taskTarget);
             store.dispatch({ type: 'ADD_TASK', payload: taskTarget });
+
+            const newTask = {
+                taskName: "New Task",
+                taskDesc: "Description (Optional)",
+                taskTarget: taskTarget
+            }
+
+            await axios.post(apiRoute, newTask)
+                .then(res => {
+                    const data = res.data;
+                    console.log(data);
+                })
+
+            await this.refresh(); // refresh so new task gets a id.
         }
 
-        const delTask = (taskId) => {
+        const delTask = async (taskId) => {
             console.log("del task click id: " + taskId);
             store.dispatch({ type: 'DEL_TASK', payload: taskId });
+
+            await axios.delete(apiRoute + taskId)
+                .then(res => {
+                    const data = res.data;
+                    console.log("deleted: ", data);
+                })
         }
 
         const cancelEdit = () => {
             store.dispatch({ type: 'EDIT_MODE', payload: -1 });
         }
 
-        const submitEdit = (data, id) => {
-            store.dispatch({ type: 'EDIT_SUBMIT', payload: { data, id } });
+        const submitEdit = async (data, id) => {
+
+            let taskIndex = findIndex(storeTasks, id);
+
+            console.log("findIDfromIndex: ", taskIndex);
+
+            store.dispatch({ type: 'EDIT_SUBMIT', payload: { data, taskIndex } });
             store.dispatch({ type: 'EDIT_MODE', payload: -1 });
+
+            try {
+                await axios.put(apiRoute + id, storeTasks[taskIndex])
+                    .then(res => {
+                        const data = res.data;
+                        //console.log(data);
+                        console.log("%cSend task index: " + taskIndex, "color:green");
+                    })
+                    .catch(err => {
+                        if (err.response.status === 404) {
+                            throw new Error('404');
+                        }
+                        throw err;
+                    })
+            } catch (err) {
+                console.log("%cCant send tasks:" + err, "color:red");
+            }
+
         }
 
-        const changeTarget = (target, id) => {
+        const changeTarget = async (target, id) => {
             store.dispatch({ type: 'CHANGE_TARGET', payload: { target, id } });
+
+            try {
+                await axios.put(apiRoute + id, storeTasks[findIndex(storeTasks, id)])
+                    .then(res => {
+                        const data = res.data;
+                        console.log(data);
+                        console.log("%cSend task index: " + findIndex(storeTasks, id), "color:green");
+                    })
+                    .catch(err => {
+                        if (err.response.status === 404) {
+                            throw new Error('404');
+                        }
+                        throw err;
+                    })
+            } catch (err) {
+                console.log("%cCant send task:" + err, "color:red");
+            }
         }
 
         const todoTasks = [], laterTasks = [], doneTasks = [];
-
-        const storeTasks = store.getState().tasks;
-        const storeEdit = store.getState().edit;
-
-        console.log(storeTasks);
 
         storeTasks.map(function (task, index) {
 
@@ -64,9 +175,9 @@ class TodoApp extends Component {
                 isEditing={storeEdit.id == index && storeEdit.editing}
                 onEditClick={() => { editMode(index) }}
                 onCancel={() => { cancelEdit() }}
-                onSubmit={(data) => { submitEdit(data, index) }}
-                onDelTask={() => { delTask(index) }}
-                onMove={(side) => { changeTarget(side, index) }}
+                onSubmit={(data) => { submitEdit(data, task.id) }} // indexi idye
+                onDelTask={() => { delTask(task.id) }}
+                onMove={(side) => { changeTarget(side, task.id) }} // indexi id ye çevirdim
             />
 
             switch (task.taskTarget) {
@@ -82,8 +193,6 @@ class TodoApp extends Component {
             }
         })
 
-        const todoCount = todoTasks.length, doneCount = doneTasks.length, laterCount = laterTasks.length;
-
         return (
             <div className={styles.wrapper}>
                 <div className={styles.content}>
@@ -94,14 +203,14 @@ class TodoApp extends Component {
                                     <FontAwesomeIcon icon={faHeartbeat} className={styles.headerIcon} /> To-Do
                                 </div>
                                 <div className={styles.count}>
-                                    {todoCount != 0 && todoCount}
+                                    {todoTasks.length != 0 && todoTasks.length}
                                 </div>
                             </div>
                         </div>
                         <div className={styles.todo_content}>
                             {todoTasks}
                             <AddTaskBtn onAddTask={() => { addTask(0) }} />
-                            {todoTasks == 0 && <AddBanner />}
+                            {todoTasks.length == 0 && <AddBanner />}
                         </div>
                     </div>
                     <div className={styles.content__done}>
@@ -111,14 +220,14 @@ class TodoApp extends Component {
                                     <FontAwesomeIcon icon={faCheckCircle} className={styles.headerIcon} /> Done
                                 </div>
                                 <div className={styles.count}>
-                                    {doneCount != 0 && doneCount}
+                                    {doneTasks.length != 0 && doneTasks.length}
                                 </div>
                             </div>
                         </div>
                         <div className={styles.done_content}>
                             {doneTasks}
                             <AddTaskBtn onAddTask={() => { addTask(1) }} />
-                            {doneTasks == 0 && <AddBanner />}
+                            {doneTasks.length == 0 && <AddBanner />}
                         </div>
                     </div>
                     <div className={styles.content__later}>
@@ -128,14 +237,14 @@ class TodoApp extends Component {
                                     <FontAwesomeIcon icon={faLightbulb} className={styles.headerIcon} /> To-Do Someday
                                 </div>
                                 <div className={styles.count}>
-                                    {laterCount != 0 && laterCount}
+                                    {laterTasks.length != 0 && laterTasks.length}
                                 </div>
                             </div>
                         </div>
                         <div className={styles.later_content}>
                             {laterTasks}
                             <AddTaskBtn onAddTask={() => { addTask(2) }} />
-                            {laterCount == 0 && <AddBanner />}
+                            {laterTasks.length == 0 && <AddBanner />}
                         </div>
                     </div>
                 </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 
 const defaultState = {
-  tasks: [
+  tasks: [/*
     {
       taskName: "Buy Groceries",
       taskDesc: "",
@@ -30,7 +30,7 @@ const defaultState = {
       taskName: "Finish the project",
       taskDesc: "Already finished. I see.",
       taskTarget: 2,
-    }
+    }*/
   ],
   edit: {
     editing: false,
@@ -41,7 +41,7 @@ const defaultState = {
 const addTaskDefault = (target) => {
   const newTask = {
     taskName: "New Task",
-    taskDesc: "Description",
+    taskDesc: "Description (Optional)",
     taskTarget: target
   }
   return newTask
@@ -55,30 +55,42 @@ export default (state = defaultState, action) => {
       return newState /*[...state.tasks, addTaskDefault(action.payload)];*/
     case 'DEL_TASK':
       
-      /* id ile sil
-      let newState = [...state];
-      newState.map(function(key, index) {
+      let theNewTasks = {...state};
+      theNewTasks.tasks.map(function(key, index) {
         console.log(key);
         if(key.id == action.payload){
-          newState.splice(index, 1);
+          theNewTasks.tasks.splice(index, 1);
         }
       })
-      */
-      // indexe göre sil
+      
+      /* indexe göre sil
       let newTasks = {...state};
-      newTasks.tasks.splice(action.payload, 1);
-      console.log(newTasks);
-      return newTasks
+      newTasks.tasks.splice(action.payload, 1);*/
+      console.log(theNewTasks);
+      return theNewTasks
     case 'CHANGE_TARGET':
       if(action.payload.target == 'left'){
-        console.log(state.tasks[action.payload.id].taskTarget);
-        if(state.tasks[action.payload.id].taskTarget > 0){
-          state.tasks[action.payload.id].taskTarget--;
-        }
+
+        //console.log(state.tasks[action.payload.id].taskTarget);
+
+        state.tasks.map(function(key, index) {
+          if(parseInt(key.id) == action.payload.id){
+            if(state.tasks[index].taskTarget > 0){
+              state.tasks[index].taskTarget--;
+            }
+          }
+        })
+        
       }else if(action.payload.target == 'right'){
-        if(state.tasks[action.payload.id].taskTarget < 2){
-          state.tasks[action.payload.id].taskTarget++;
-        }
+
+        state.tasks.map(function(key, index) {
+          if(parseInt(key.id) == action.payload.id){
+            if(state.tasks[index].taskTarget < 2){
+              state.tasks[index].taskTarget++;
+            }
+          }
+        })
+        
       }
       return state
     case 'EDIT_MODE':
@@ -87,15 +99,19 @@ export default (state = defaultState, action) => {
       state.edit.id = action.payload;
       return state
     case 'EDIT_SUBMIT':
-      console.log(action.payload.id);
+      console.log("state tasks: ", state.tasks);
       if(action.payload.data.title){
-        state.tasks[action.payload.id].taskName = action.payload.data.title;
+        state.tasks[action.payload.taskIndex].taskName = action.payload.data.title;
         console.log("changed title to: "+action.payload.data.title);
       }
       if(action.payload.data.desc){
-        state.tasks[action.payload.id].taskDesc = action.payload.data.desc;
+        state.tasks[action.payload.taskIndex].taskDesc = action.payload.data.desc;
         console.log("changed desc to: "+action.payload.data.desc);
       }
+      return state
+    case 'SET_TASKS':
+      state.tasks = action.payload;
+      console.log("updated: ", state);
       return state
     default:
       return state
